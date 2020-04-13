fca02f43ef3a1928fa6b6f443c877a813061e1ce
Acedia <adhan.scorpion99@gmail.com>
2020-04-14 04:24:39 +0700

    Add main menu and game scene

---
+++ PolyrealmJam/Assets/Scripts/PlayerGraphics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGraphics : MonoBehaviour
+{
+    private PlayerMovement movement;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (movement.input > 0f)
+            spriteRenderer.flipY = true;
+
+        if (movement.input < 0f)
+            spriteRenderer.flipY = false;
+    }
+}
---
+++ PolyrealmJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2f;
+
+    public float input { get; private set; }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        input = 0f;
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            input += 1f;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            input -= 1f;
+
+        Move(input);
+    }
+
+    private void Move(float dir)
+    {
+        transform.Translate(Vector2.right * dir * speed * Time.deltaTime);
+    }
+}
